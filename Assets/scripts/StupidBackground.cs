4de1b27e64bdea37fd794ee66d597bc88238523a
PotatingSloth <mintingtu@gmail.com>
2021-10-17 02:26:29 -0400

    Background Animation
    Got the background to animate and loop seemlessly. It's still in the test scene so you gotta hook it up in the real scene if you want it to be there.

---
+++ Assets/scripts/StupidBackground.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StupidBackground : MonoBehaviour
+{
+    public Sprite[] frames;
+    private float Timer;
+    private int CurrentFrame;
+    public SpriteRenderer BackgroundRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+
+        if(Timer >= 0.04f)
+        {
+            CurrentFrame += 1;
+            if(CurrentFrame >= frames.Length)
+            {
+                CurrentFrame = 0;
+            }
+            Timer = 0f;
+        }
+
+        BackgroundRenderer.sprite = frames[CurrentFrame];
+    }
+}
